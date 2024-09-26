169804d9f7fe56f00d6eef27c444d5fad5d86bd9
THOMio2565 <thomio.lacroix@gmail.com>
2024-09-26 16:31:30 +0200

    [UPDATE] trigonometrie.py
    Ajout des fonctions sinus, cosinus et tangeante

---
+++ supermath/trigonometrie.py
@@ -0,0 +1,14 @@
+import math
+
+def sinus(x):
+    return math.sin(x)
+
+def cosinus(x):
+    return math.cos(x)
+
+def tangeante(x):
+    return math.tan(x)
+
+print(round(sinus(math.pi), 2))
+print(cosinus(math.pi*2))
+print(round(tangeante(math.pi*2), 2))
